68b6a584957e12048052ad5dc66b97322a1fa24a
Joachim Astel <achim@astel.de>
2018-12-23 18:19:10 +0100

    pysmbus throws errors, smbus2 will fit
    error with pysmbus was: TypeError("Object given for i2c_bus must implement write_i2c_block_data and read_i2c_block_data")

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 # this is my trial and error script for implementing it anyway
 
 apk add python3 python3-dev gcc libc-dev
-pip3 install automationhat pysmbus
+pip3 install automationhat smbus2
 mount -o remount,rw /media/mmcblk0p1
 echo "dtparam=i2c_arm=on" >> /media/mmcblk0p1/usercfg.txt
 echo "i2c-dev" >> /etc/modules-load.d/i2c-dev.conf
