c72e9257d4f14cac488895917f6b0036112534f3
Joachim Astel <achim@astel.de>
2018-12-23 17:30:25 +0100

    initialize /dev/i2c-1 at boot time

--- setup.sh
+++ setup.sh
@@ -7,4 +7,5 @@ apk add python3 python3-dev gcc libc-dev
 pip3 install automationhat pysmbus
 mount -o remount,rw /media/mmcblk0p1
 echo "dtparam=i2c_arm=on" >> /media/mmcblk0p1/usercfg.txt
+echo "modprobe i2c-dev" >> /etc/rc.local
 lbu commit
