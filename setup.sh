3085759ab52f404725c9adc22d6525451dca4d9a
Joachim Astel <achim@astel.de>
2018-12-23 17:48:17 +0100

    include /etc and /usr/lib/python3.6 to lbu

--- setup.sh
+++ setup.sh
@@ -7,5 +7,7 @@ apk add python3 python3-dev gcc libc-dev
 pip3 install automationhat pysmbus
 mount -o remount,rw /media/mmcblk0p1
 echo "dtparam=i2c_arm=on" >> /media/mmcblk0p1/usercfg.txt
-echo "modprobe i2c-dev" >> /etc/rc.local
+echo "i2c-dev" >> /etc/modules-load.d/i2c-dev.conf
+lbu include /usr/lib/python3.6
+lbu include /etc
 lbu commit
