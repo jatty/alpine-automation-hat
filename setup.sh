453973d73dae8fefb38a3c722e9a163a7c5f5107
Joachim Astel <achim@astel.de>
2018-12-23 17:28:55 +0100

    make filesystem r/w
    /media/mmcblk0p1 may be read-only by default

--- setup.sh
+++ setup.sh
@@ -5,5 +5,6 @@
 
 apk add python3 python3-dev gcc libc-dev
 pip3 install automationhat pysmbus
+mount -o remount,rw /media/mmcblk0p1
 echo "dtparam=i2c_arm=on" >> /media/mmcblk0p1/usercfg.txt
 lbu commit
